772562ef867713dc0e986b1094717c715c1b2917
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-01-23 22:17:34 +0600

    first commit

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+// skills
+document.addEventListener("DOMContentLoaded", () => {
+    const circles = document.querySelectorAll(".circle-container");
+  
+    circles.forEach((circle) => {
+      const percentage = circle.getAttribute("data-percentage");
+      circle.style.setProperty("--percentage", percentage);
+    });
+  });
